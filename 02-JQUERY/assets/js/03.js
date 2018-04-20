00a622add4c574f5ccd4f12701a38b5cc33fb291
Pransun <38323441+Pransun@users.noreply.github.com>
2018-04-20 16:44:30 +0200

    20/04/2018

---
+++ 02-JQUERY/assets/js/03.js
@@ -0,0 +1,26 @@
+/*--------------------------------------------------
+    LE CHAINAGE DE METHODE AVEC JQUERY  
+---------------------------------------------------*/
+
+$(function() {
+
+    console.log($('div'));
+    
+    // -- Je souhaite cacher toutes les div de ma page HTML
+    $('div').hide('slow', function() {
+
+        /**
+         * Une fois que la méthode hide () est terminée, mon alerte peut s'excuter : 
+         *      NOTA BENE : La fonction s'executera pour l'ensemble des éléments du sélecteur.
+         */
+        // alert('fin du Hide');
+        // $('div').css('background', 'yellow');
+        // $('div').css('color', 'red');
+        // $('div').show('slow');
+        
+        //$('p').hide(1000).css('color','blue').css('font-size','20px').delay(2000).show(500);
+        
+        $('p').hide(1000).css({'color': 'blue', 'font-size': '20px'}).delay(2000).show(500);
+    }); // fin du hide()
+
+}); // fin de $()//
