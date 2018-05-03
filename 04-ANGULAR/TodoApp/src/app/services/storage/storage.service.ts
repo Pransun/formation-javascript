1fccecbf7b9c731e045f2f4ea3e34b8bd5054259
Pransun <38323441+Pransun@users.noreply.github.com>
2018-05-03 16:30:47 +0200

    03/05/2018

---
+++ 04-ANGULAR/TodoApp/src/app/services/storage/storage.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {Task} from '../../models/task';
+
+@Injectable()
+export class StorageService {
+
+  constructor() { }
+
+  /**
+   * Permet de sauvegarder les tâches dans le localStorage.
+   * @param {Task[]} tasks
+   */
+  save(tasks: Task[]) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  /**
+   * Retourne une liste de tâches depuis le localstorage ou, un tableau vide.
+   */
+  getTasks() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks !== null) {
+      return tasks;
+    } else {
+      return [];
+    }
+  }
+}
